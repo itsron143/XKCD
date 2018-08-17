452b4faa481859bc6192485a89210ba60728f502
Rounak Vyas <itsron143@gmail.com>
2018-08-18 01:59:08 +0530

    Update xkcd.py

--- xkcd.py
+++ xkcd.py
@@ -1,32 +1,41 @@
 import os
 import click
+from sh import lolcat, figlet
 import json
 import urllib.request
+import json
+import urllib
 from PIL import Image
 import requests
 from io import BytesIO
 from random import randint
-import sh
-from sh import lolcat, figlet
+
 
 @click.command()
 @click.option('--random', flag_value='random', default=False, help='Get Random Comic!')
 def cli(random):
-    """XCKD Terminal Tool"""
+    """XKCD Terminal Tool"""
     #click.echo('Hello World!')
     # os.system('figlet -c "X K C D" | lolcat')
-    print(lolcat(figlet("-c", "X K C D")))
     # print(random)
+    try:
+        print(lolcat(figlet("-c", "X K C D")))
+    except ImportError:
+        print("Welcome to xkcd Comics!")
     rand_digits = str(randint(100, 999))
-    if random == 'random':
-        with urllib.request.urlopen("https://xkcd.com/" + rand_digits + "/info.0.json") as url:
-            data = json.loads(url.read().decode())
-            response = requests.get(data['img'])
-            img = Image.open(BytesIO(response.content))
-            img.show()
-    else:
-        with urllib.request.urlopen("https://xkcd.com/info.0.json") as url:
-            data = json.loads(url.read().decode())
-            response = requests.get(data['img'])
-            img = Image.open(BytesIO(response.content))
-            img.show()
+    try:
+        if random == 'random':
+            with urllib.request.urlopen("https://xkcd.com/" + rand_digits + "/info.0.json") as url:
+                data = json.loads(url.read().decode())
+                response = requests.get(data['img'])
+                img = Image.open(BytesIO(response.content))
+                img.show()
+        else:
+            with urllib.request.urlopen("https://xkcd.com/info.0.json") as url:
+                data = json.loads(url.read().decode())
+                response = requests.get(data['img'])
+                img = Image.open(BytesIO(response.content))
+                img.show()
+    except urllib.error.URLError:
+        error_image = Image.open("assets/xkcd_404.jpg")
+        error_image.show()
